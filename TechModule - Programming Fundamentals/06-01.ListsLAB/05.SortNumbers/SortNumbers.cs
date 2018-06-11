850a0fbee5e88234a8c094af5b903ca5c32a15e6
Silvestar Kamenov <sdkamenov@gmail.com>
2018-06-11 22:17:41 +0200

    lists lab

---
+++ TechModule - Programming Fundamentals/06-01.ListsLAB/05.SortNumbers/SortNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortNumbers
+{
+    class SortNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+            list.Sort();
+
+            Console.WriteLine(string.Join(" <= ", list));
+        }
+    }
+}
---
+++ TechModule - Programming Fundamentals/06-01.ListsLAB/07.CountNumbers/CountNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountNumbers
+{
+    class CountNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> counts = new List<int>();
+            numbers.Sort();
+
+            int count = 1;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i]} -> {count}");
+                    count = 1;
+                }
+            }
+            Console.WriteLine($"{numbers[numbers.Count - 1]} -> {count}");
+
+        }
+    }
+}
