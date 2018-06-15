aab2a973445a6c0b05db0dff1bd9bc9bcc4f98ec
Silvestar Kamenov <sdkamenov@gmail.com>
2018-06-15 03:31:53 +0200

    listsEX

---
+++ TechModule - Programming Fundamentals/06.ListsEXERCISES/06.SumReversedNumbers/SumReversedNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumReversedNumbers
+{
+    class SumReversedNumbers
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(Console.ReadLine()
+                .Split()
+                .Select(a => int.Parse(new string(a.Reverse().ToArray())))
+                .Sum());
+            
+            //List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    List<char> num = numbers[i].ToString().ToList();
+            //    num.Reverse();
+            //    sum += int.Parse(string.Join("", num));
+            //}
+            //Console.WriteLine(sum);
+        }
+    }
+}
---
+++ TechModule - Programming Fundamentals/06.ListsEXERCISES/07.BombNumbers/BombNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BombNumbers
+{
+    class BombNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] bombNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int bombNumber = bombNums[0];
+            int range = bombNums[1];
+            int bombIndex = nums.IndexOf(bombNumber);
+
+            while (bombIndex != -1)
+            {
+                int leftIndex = bombIndex - range;
+                int rightIndex = bombIndex + range;
+
+                if (leftIndex < 0)
+                {
+                    leftIndex = 0;
+                }
+                if (rightIndex > nums.Count - 1)
+                {
+                    rightIndex = nums.Count - 1;
+                }
+
+                int count = rightIndex - leftIndex+1;
+                nums.RemoveRange(leftIndex, count);
+
+                bombIndex = nums.IndexOf(bombNumber);
+            }
+
+            int sum = 0;
+            foreach (var num in nums)
+            {
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
