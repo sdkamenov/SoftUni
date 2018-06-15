01db1ce93803e5a1aa627e6e1b8fee858db64d44
Silvestar Kamenov <sdkamenov@gmail.com>
2018-06-15 17:23:07 +0200

    dicts, lambda, linq

---
+++ TechModule - Programming Fundamentals/07-01.DictionariesLambdaLinqLAB/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrences
+{
+    class OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Select(s=>s.ToLower()).ToArray();
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, 1);
+                }
+                else
+                {
+                words[word]++;
+                }
+            }
+
+            List<string> oddCount = new List<string>();
+            foreach (var kvp in words)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    oddCount.Add(kvp.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ",  oddCount));
+        }
+    }
+}
