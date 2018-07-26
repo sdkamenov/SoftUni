20f3e9002480bef90c5d44a3c78bb14059730902
Silvestar Kamenov <sdkamenov@gmail.com>
2018-07-26 04:55:18 +0200

    php symfony blog

---
+++ 02.SoftTech/08.PHPBlog/README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on July 25, 2018, 4:15 pm.
---
+++ 02.SoftTech/08.PHPBlog/src/MyBlogBundle/Form/ArticleType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MyBlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MyBlogBundle\Entity\Article'
+        ));
+    }
+}
