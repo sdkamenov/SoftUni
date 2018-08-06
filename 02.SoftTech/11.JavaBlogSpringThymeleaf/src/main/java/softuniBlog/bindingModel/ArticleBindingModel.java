807270197965f8389499584d38776f9d155c6f3c
Silvestar Kamenov <sdkamenov@gmail.com>
2018-08-06 17:45:19 +0200

    java blog

---
+++ 02.SoftTech/11.JavaBlogSpringThymeleaf/src/main/java/softuniBlog/bindingModel/ArticleBindingModel.java
@@ -0,0 +1,30 @@
+package softuniBlog.bindingModel;
+
+import javax.validation.constraints.NotNull;
+
+public class ArticleBindingModel {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NotNull String title) {
+        this.title = title;
+    }
+
+    @NotNull
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(@NotNull String content) {
+        this.content = content;
+    }
+}
